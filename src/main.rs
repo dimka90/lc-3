dd93c5066fb173d7abff90f818cc4e9eb129d5d2
dimka90 <dimkayilrit@gmail.com>
2025-09-05 09:14:57 +0100

    refactor: refactore the mode into modular structure

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::{io::ErrorKind, u16};
+
 #[derive(Debug)]
 #[repr(u16)]
- enum Registers{
+enum Registers {
     R0 = 0,
     R1,
     R2,
@@ -11,82 +13,114 @@
     R7,
     PC,
     COND,
-    COUNT
-    }
+    COUNT,
+}
 
 #[derive(Debug)]
-enum Opcodes
-{
+enum Opcodes {
     BR,
-    ADD,    /* add  */
-    LD,     /* load */
-    ST,     /* store */
-    JSR,    /* jump register */
-    AND,    /* bitwise and */
-    LDR,    /* load register */
-    STR,    /* store register */
-    RTI,    /* unused */
-    NOT,    /* bitwise not */
-    LDI,    /* load indirect */
-    STI,    /* store indirect */
-    JMP,    /* jump */
-    RES,    /* reserved (unused) */
-    LEA,    /* load effective address */
-    TRAP    /* execute trap */
+    ADD,  /* add  */
+    LD,   /* load */
+    ST,   /* store */
+    JSR,  /* jump register */
+    AND,  /* bitwise and */
+    LDR,  /* load register */
+    STR,  /* store register */
+    RTI,  /* unused */
+    NOT,  /* bitwise not */
+    LDI,  /* load indirect */
+    STI,  /* store indirect */
+    JMP,  /* jump */
+    RES,  /* reserved (unused) */
+    LEA,  /* load effective address */
+    TRAP, /* execute trap */
 }
 
 #[derive(Debug)]
 
-enum Flags
-{
+enum Flags {
     POS = 1 << 0, /* P */
     ZRO = 1 << 1, /* Z */
     NEG = 1 << 2, /* N */
 }
 
-
-const MEMORY_MAX: u16 = u16::MAX;
-// const MEMORY_MAX : u16 = ((1 << 16) - 1) as u16;
-// const MEMORY_MAX : usize = 1 << 16;
-
+const MEMORY_MAX: usize = 1 << 16;
+const REG_LOCATIONS: u16 = 10;
+const PC_START: u16 = 0x3000;
 
 #[derive(Debug)]
 struct Memory {
-    locations: [u16; MEMORY_MAX as usize]
+    locations: [u16; MEMORY_MAX as usize],
 }
 
-impl Memory {
+#[derive(Debug)]
+struct Register {
+    locations: [u16; REG_LOCATIONS as usize],
+}
+
+trait RegisterTrait {
+    fn new() -> Self;
+    fn load(&self, reg_location: Registers) -> Option<u16>;
+    fn store(&mut self, instr: u16, reg_location: Registers) -> Result<(), ErrorKind>;
+}
+
+trait MemoryTrait {
+    fn new() -> Self;
+    fn mem_read(&self, register: &mut Register) -> Option<u16>;
+}
+
+impl RegisterTrait for Register {
     fn new() -> Self {
         Self {
-            locations: [0u16; MEMORY_MAX as usize]
+            locations: [0u16; REG_LOCATIONS as usize],
         }
     }
 
-    
+    fn load(&self, reg_location: Registers) -> Option<u16> {
+        let data = self.locations[reg_location as usize];
+        return Some(data);
+    }
+    fn store(&mut self, instr: u16, reg_location: Registers) -> Result<(), ErrorKind> {
+        self.locations[reg_location as usize] = instr;
+        return Ok(());
+    }
 }
-// const MEMORY_MAX : u16 = ((1u32 << 16) - 1 as u16);
 
+impl MemoryTrait for Memory {
+    fn new() -> Self {
+        Self {
+            locations: [0u16; MEMORY_MAX as usize],
+        }
+    }
+
+    fn mem_read(&self, register: &mut Register) -> Option<u16> {
+        let mem_addr = register.locations[Registers::PC as usize];
+        let _ = register.store(mem_addr + 1, Registers::PC);
+        Some(self.locations[mem_addr as usize])
+    }
+}
 
 fn main() {
-    // let MEMORY_MAX : u16 = ((1u32 << 16) - 1).try_into().unwrap();
-
-    const PC_START: u16 = 0x3000;
-    
-    let  mut reg_storage: [u16; Registers::COUNT as usize] =  [0; Registers::COUNT as usize];
-    
-    let ZERO_FLAG = Flags::ZRO as u16;
-    let register_cond = Registers::COND as usize;
-
-    reg_storage[register_cond] = ZERO_FLAG;
-   
-
-    reg_storage[Registers::PC as usize] = PC_START;
-    // println!("Registers {:?}", reg_storage);
     let mut memory = Memory::new();
+    let mut register = Register::new();
 
-    // memory.Location.push(8);
-    println!("{:?}", memory.locations[MEMORY_MAX as usize]);
+    memory.locations[12288] = 0x3000;
+    let _ = register.store(PC_START, Registers::PC);
+    let instr = memory.mem_read(&mut register).unwrap();
+
+    let opcode = instr >> 12;
 
 
+    println!("{register:?}");
+     println!("{:#01x}", 12288);
+     println!("{opcode}");
+     println!("instruction {instr:#01x}");
+    
 
+    match opcode {
+        1 => println!("Addition Operation"),
+        2 => println!("AND operation"),
+        3 => println!("NOT operation"),
+        _ => println!("Unknown.......")
+    }
 }
